81dc16031e5fb9da0b0a4fc4677a1d1b42ac8b70
Joel Wisa <jwbc5@umsystem.edu>
2021-02-25 14:15:47 -0600

    added functionality to move single block

--- JoelTetris.py
+++ JoelTetris.py
@@ -6,6 +6,7 @@ from tkinter import messagebox
 
 colors = [(0,255,255),(255,60,245),(255,45,45),(55,255,65),(245,255,50),(255,180,195)]
 
+
 #Play zone
 play_zone_W   = 10
 play_zone_H   = 20
@@ -173,3 +174,92 @@ class Tetris:
 
     def stop(self): #stops shape from moving whether ticking down or collision
         pass
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self,start,dirnx=0,dirny=1,color=(255,0,0)):
+        self.pos = start
+        self.dirnx = 0
+        self.dirny = 1
+        self.color = color
+
+    def move(self):
+        keypress = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    keypress = True
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    keypress = True
+        if self.pos[1] >= self.rows-1:
+            pass
+        elif keypress:
+            if self.dirnx == -1 and self.pos[0] <= 0:
+                pass
+            elif self.dirnx == 1 and self.pos[0] >= self.rows-1:
+                pass
+            else:
+                self.pos = (self.pos[0] + self.dirnx, self.pos[1])
+        else:
+            self.pos = (self.pos[0], self.pos[1] + 1)
+
+
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+    
+        pygame.draw.rect(surface, self.color, (i*dis+1,j*dis+1, dis-2, dis-2))
+
+#def shape()
+
+def randomSnack(rows):
+
+    x = random.randrange(rows)
+    y = 0
+        
+    return (x,y)
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
+
+def main():
+    width = 500
+    height = 500
+    rows = 20
+    run = True
+    snack = cube(randomSnack(rows), color=(0,255,0))
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((width, height))
+
+    while run:
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        if not(snack.pos[1] >= snack.rows-1):
+            snack.move()
+
+        window.fill((0,0,0))
+        snack.draw(window)
+        drawGrid(width,rows, window)
+        pygame.display.update()
+        
+main()
