21d7e7ffc9f7a4b0c91f137c34864964d32ce880
josephrpeak <jrp3yp@umsystem.edu>
2021-03-23 23:15:11 -0500

    Fine tuned some display variables. draw_grid() implemented. Cleaned up comments a bit.

--- JoelTetris.py
+++ JoelTetris.py
@@ -5,13 +5,17 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 
-width = 800 #Screen
-height = 700 #Screen
-play_width = 300 #Play zone
-play_height = 600 #Play zone
-square_size = 30 #Size per block
+width = 800 # Display width
+height = 700 # Display height
+play_width = 300 # Play zone
+play_height = 600 # Play zone
+square_size = 30 # Size per block
 rows = 20
 cols = 10
+
+# Color constants
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 #Determines the upper left block of the play zone grid
 #for the play zone to be in the middle of the screen
@@ -39,6 +43,22 @@ def create_grid(stopped_pieces = {}):
                 grid[i][j] = stopped
     return grid
 
+# Draws crosshatched pattern on canvas
+def draw_grid(window):
+    x, y = 0, 0
+
+    # Draw horizontal lines
+    for _ in range(rows+1):
+        pygame.draw.line(window, WHITE, [play_width, x], [2*play_width, x], 1)
+        x += square_size
+    # Draw vertical lines
+    for _ in range(cols+1):
+        pygame.draw.line(window, WHITE, [y+play_width, 0], [y+play_width, play_height], 1)
+        y += square_size
+
+        
+
+
 #puts shape into array (current plan involves active shape being marked by 2's on array)
 #def shape(<indicate what shape>):
 
@@ -55,23 +75,41 @@ def create_grid(stopped_pieces = {}):
 #def drop(<indicate fast or slow>):
 
 def main():
+
+    # Initialization stuff
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Joel Tetris.inc")
+    clock = pygame.time.Clock()
+    game_over = False
+
     stopped_pieces = {} #Dictionary for pieces placed and not moving on grid in form
-                        # (x,y): 0,1,2 where 0-no block, 1-curr block moving, 2-block done moving
+                            # (x,y): 0,1,2 where 0-no block, 1-curr block moving, 2-block done moving
 
-    run = True
-    while run:
-        collision = False
-        #shape()
-        while not(collision):
-            pass
-            #take keyboard inputs here
-            #rotate()
-            #move()
-            #drop()
-    print(sa)
-    #put display stuff here
+    # Game loop
+    while not game_over:
+ 
+        # Ten ticks per second
+        clock.tick(10)
+         
+        # Event handling (user input)
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                game_over = True 
+     
+         
+        # Clear the screen and set the screen background
+        window.fill(BLACK)
 
-window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Joel Tetris.inc")
+        # Draw crosshatched pattern on window
+        draw_grid(window)
+
+        pygame.display.flip()
+
+        #rotate()
+        #move()
+        #drop()
+        #print(sa)
+
+    pygame.quit()
 
 main()
