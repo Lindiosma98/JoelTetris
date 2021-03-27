f6edaec9769f6ba8a117dfb1a3089505c509d0b2
josephrpeak <jrp3yp@umsystem.edu>
2021-03-26 23:15:28 -0500

    Left and right movement implemented, but needs modification for every block type except for O. A binary representation of the game board prints to console and it updates according to what is drawn to the screen. Got rid of unnecessary code.

--- JoelTetris.py
+++ JoelTetris.py
@@ -21,72 +21,83 @@ margin = 50
 colors = [(0,0,0),(255,255,255),(0,255,255),(255,60,245),(255,45,45),(55,255,65),(245,255,50)]
 bg_color = (125, 150, 175)
 
+board = np.zeros((20, 10))
+
 # Numpy arrays for shapes. 4x4. Column-major.
-T = np.array([(0,1,0,0),(1,1,0,0),(0,1,0,0),(0,0,0,0)])
-O = np.array([(1,1,0,0),(1,1,0,0),(0,0,0,0),(0,0,0,0)])
-J = np.array([(0,0,1,0),(1,1,1,0),(0,0,0,0),(0,0,0,0)])
-L = np.array([(0,0,0,0),(1,1,1,0),(0,0,1,0),(0,0,0,0)])
-Z = np.array([(1,0,0,0),(1,1,0,0),(0,1,0,0),(0,0,0,0)])
-S = np.array([(0,1,0,0),(1,1,0,0),(1,0,0,0),(0,0,0,0)])
-I = np.array([(0,1,0,0),(0,1,0,0),(0,1,0,0),(0,1,0,0)])
+T = np.array([
+    (0,1,0,0),
+    (1,1,0,0),
+    (0,1,0,0),
+    (0,0,0,0)
+    ])
+O = np.array([
+    (1,1,0,0),
+    (1,1,0,0),
+    (0,0,0,0),
+    (0,0,0,0)
+    ])
+J = np.array([
+    (0,0,1,0),
+    (1,1,1,0),
+    (0,0,0,0),
+    (0,0,0,0)
+    ])
+L = np.array([
+    (0,0,0,0),
+    (1,1,1,0),
+    (0,0,1,0),
+    (0,0,0,0)
+    ])
+Z = np.array([
+    (1,0,0,0),
+    (1,1,0,0),
+    (0,1,0,0),
+    (0,0,0,0)
+    ])
+S = np.array([
+    (0,1,0,0),
+    (1,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0)
+    ])
+I = np.array([
+    (0,1,0,0),
+    (0,1,0,0),
+    (0,1,0,0),
+    (0,1,0,0)
+    ])
 
 # Index this to access one of the np arrays above. i.e. "Shapes[2]" would equal J
 Shapes = [T, O, J, L, Z, S, I]
 
-class Shape:
-
-    # The minimum and maximum values a piece can have for its coordinates: (450, 50), (750, 650)
-    left_bound = 450
-    right_bound = 750
-    up_bound = 50
-    down_bound = 650
-
-    '''def __init__(self, width, height, shape, color):
-        self.x = width
-        self.y = height
-        self.shape = shape
-        self.color = color
-        self.rotation = 0 #Base orientation '''
-
-    '''def draw(window, x, y):
-        pygame.draw.rect(window, colors[2], (x, y, square_size, square_size))'''
-
-
-def drawSquare(window, x, y):
-        pygame.draw.rect(window, colors[2], (x, y, square_size, square_size))
-
-
-# Currently, this is the function that is used to display a shape. Could be streamlined by implementing the drawSquare method above into it. Easy fix.
-def fillSquares(window, arr, rows, cols):
-    for i in range(rows):
-        for j in range(cols):
+# Places the first shape of the game. Should be randomly generated, but that can't happen until all shapes can be moved without a segfault.
+def placeStartingShape(window, board):
+    #shape = Shapes[random.randrange(len(Shapes)-1)]
+    shape = Shapes[1]
+    for i in range(shape.shape[0]):
+        for j in range(shape.shape[1]):
             # only draw squares in cells populated with a 1
-            if(arr[i][j] == 1):
-                # params: (window name, x coordinate, y coordinate)
-                drawSquare(window, arr[i][j] * square_size * i + 450, arr[i][j] * square_size * j + margin)
-            
+            if(shape[i][j] == 1):
+                board[i][j] = 1
 
+# Fill the squares on the grid with color. Only fills cells populated with a 1
+def drawShapes(window, board):
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            if(board[i][j] == 1):
+                #print(j,i)
+                #print(j*square_size+450, i*square_size+margin)
+                pygame.draw.rect(window, colors[2], (j*square_size+450, i*square_size+margin, square_size, square_size))
 
-#squareArray = [[0]*cols]*rows
-#0 = no block, 1 = stationary block, 2 = active shape
-#sa = np.array(squareArray)
-#collision = False
+# This needs to be implemented correctly...this ain't the way.
+'''def fall(window, board):
+    for i in range(board.shape[0]-1):
+        for j in range(board.shape[1]-1):
+            if(board[i][j] == 1):
+                board[i+1][j] = 1
+                break'''
 
-#Create class for pieces
-
-#Create grid 10x20 that has 0 throughout to show no blocks
-#stopped_pieces keeps track of spaces of pieces that are
-#locked into the grid by game collision rules
-def create_grid(stopped_pieces = {}):
-    grid = [[0 for _ in range(10)] for _ in range(20)]
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (i,j) in stopped_pieces:
-                stopped = stopped_pieces[(i,j)]
-                grid[i][j] = stopped
-    return grid
-
+                
 # Draws crosshatched pattern on canvas
 def draw_grid(window):
     x, y = 0, 0
@@ -103,61 +114,122 @@ def draw_grid(window):
         pygame.draw.line(window, colors[0], [width/2+x+margin, 0+margin], [width/2+x+margin, height-margin], 1)
         x += square_size
 
-        
 
+def move(window, dir):
+    # simple list of shape coordinate values (aka where the 1's are in the board)
+    coords = []
 
-#puts shape into array (current plan involves active shape being marked by 2's on array)
-#def shape(<indicate what shape>):
+    # the same list as above, but converted into list of lists (each of length 2 for easy and sensible indexing)
+    pairs = []
+    
+    # grabbing max row and col values
+    rows, cols = board.shape[0], board.shape[1]
+    
+    # loop through board array, store locations of 1's (aka where the shape currently is located)
+    for i in range(rows):
+        for j in range(cols):
+            if(board[i][j] == 1):
+                coords.append(i)
+                coords.append(j)
+                pairs.append(list(coords))
+                coords.pop()
+                coords.pop()
 
-#rotates shape
-#def rotate(<indicate direction>):
+    #print(pairs)
+    
+    # This strategy of block movement currently only works for the O block (2x2). Segfaults otherwise.
+    if(dir == "L"):
+        # Only move left if the leftmost parts of the block aren't at index 0 of the board
+        if(pairs[0][1] != 0 and pairs[2][1] != 0):
+            board[pairs[0][0]][pairs[0][1]-1] = 1
+            board[pairs[2][0]][pairs[2][1]-1] = 1
+            board[pairs[1][0]][pairs[1][1]] = 0
+            board[pairs[3][0]][pairs[3][1]] = 0
+    elif(dir == "R"):
+        # Only move right if the rightmost parts of the block aren't at index 9 of the board
+        if(pairs[1][1] != 9 and pairs[3][1] != 9):
+            board[pairs[1][0]][pairs[1][1]+1] = 1
+            board[pairs[3][0]][pairs[3][1]+1] = 1
+            board[pairs[0][0]][pairs[0][1]] = 0
+            board[pairs[2][0]][pairs[2][1]] = 0
+    elif(dir == "D"):
+        pass
 
-#def move(<indicate direction>):
+    # Empty the coordinate lists for the next call to this function
+    pairs.clear()
+    coords.clear()
 
-#def drop(<indicate fast or slow>):
 
 def main():
 
     # Initialization stuff
     window = pygame.display.set_mode((width, height))
-    pygame.display.set_caption("Joel Tetris.inc")
+    pygame.display.set_caption("Joel Tetris")
     clock = pygame.time.Clock()
     game_over = False
+    random.seed()
+    dirx = ""
 
-    stopped_pieces = {} #Dictionary for pieces placed and not moving on grid in form
-                            # (x,y): 0,1,2 where 0-no block, 1-curr block moving, 2-block done moving
+    # Place the first shape
+    placeStartingShape(window, board)
 
     # Game loop
     while not game_over:
- 
-        # Ten ticks per second
-        clock.tick(10)
-         
+        pygame.time.delay(100)
+        # One hundred ticks per second
+        clock.tick(100)
+        
         # Event handling (user input)
+        pygame.key.set_repeat(1, 10) 
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT: 
                 game_over = True 
-     
+
+            keys = pygame.key.get_pressed()
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    #print("Left")
+                    dirx = "L"
+                elif keys[pygame.K_RIGHT]:
+                    #print("Right")
+                    dirx = "R"
+
+        # Move the block in the direction dirx
+        move(window, dirx)
+        dirx = ""
+        #fall(window, board)
          
         # Clear the screen and set the screen background
         window.fill(bg_color)
 
-        # Store cell states (0 = no block, 1 = current block moving, 2 = block done moving)
-        stopped_pieces = create_grid(stopped_pieces)
+        # Print the board state in console
+        print(board)
 
-        # Demonstrating drawing a block to screen. Change "T" to another shape name to display corresponding shape
-        fillSquares(window, Shapes[1], Shapes[1].shape[0], Shapes[1].shape[1])
+        # Draw shape to screen
+        drawShapes(window, board)
 
         # Draw crosshatched pattern on window
         draw_grid(window)
-    
         pygame.display.flip()
 
         #rotate()
         #move()
         #drop()
         #print(sa)
+        #stopped_pieces = {} #Dictionary for pieces placed and not moving on grid in form
+                            # (x,y): 0,1,2 where 0-no block, 1-curr block moving, 2-block done moving
+        #Create grid 10x20 that has 0 throughout to show no blocks
+        #stopped_pieces keeps track of spaces of pieces that are
+        #locked into the grid by game collision rules
+        '''def create_grid(stopped_pieces = {}):
+            grid = [[0 for _ in range(10)] for _ in range(20)]
 
+            for i in range(len(grid)):
+                for j in range(len(grid[i])):
+                    if (i,j) in stopped_pieces:
+                        stopped = stopped_pieces[(i,j)]
+                        grid[i][j] = stopped
+            return grid'''       
     pygame.quit()
 
 main()
